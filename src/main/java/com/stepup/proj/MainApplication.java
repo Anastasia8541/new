ffbd70b6ccd42afdc808bc591e687ddd7bfdfb33
Anastasia N <git.user.nashko2013@yandex.ru>
2025-11-16 12:23:59 +0300

    Доработка логики проекта

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,6 +2,8 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Привет");
+        System.out.println("tttt");
+        System.out.println("jjj");
+
     }
 }
